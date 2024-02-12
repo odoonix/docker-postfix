be3ce9a8c8ede9b6cd3c75cd48c6e479c09b41f1
YasinFM <mohammadyasinfarshad@gmail.com>
2024-02-12 20:20:36 +0330

    [update] added unit tests for fil_cp() and file_mv()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import random
 import string
 import unittest
+import os
 
 from api import utils
 
@@ -11,5 +12,18 @@ class TestUtils(unittest.TestCase):
         utils.linux.file_wirte(data, 'a.txt')
         result = utils.linux.file_read('a.txt')
         self.assertEqual(result, data)
-
         
+    def test_copy(self):
+        src = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
+        utils.linux.file_wirte(src, 'a.txt')
+        utils.linux.file_cp('a.txt', 'b.txt')
+        dst = utils.linux.file_read('b.txt')
+        self.assertEqual(src,dst)
+        
+    def test_move(self):
+        src = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
+        utils.linux.file_wirte(src, 'a.txt')
+        utils.linux.file_mv('a.txt', 'b.txt')
+        dst = utils.linux.file_read('b.txt')
+        if not os.path.isfile('a.txt'):
+            self.assertEqual(src,dst)
